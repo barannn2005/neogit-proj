d81ce7c5d7265a4501064328339c3f002a9a1fcc
baran <baranhoseini.cloudstear@gmail.com>
2024-01-28 18:37:33 -0600

    the init part updatd

--- 1.c
+++ 1.c
@@ -1,44 +1,57 @@
 #include <stdio.h>
 #include <windows.h>
 
-int init_exist(const char *path)
+int initexist(const char *path)
 {
     DWORD attributes = GetFileAttributes(path);
     return (attributes != INVALID_FILE_ATTRIBUTES && (attributes & FILE_ATTRIBUTE_DIRECTORY));
+}
+int initcheckup(const char *currentDirectory)
+{
+    char neogitPath[MAX_PATH];
+    while (1)
+    {
+        snprintf(neogitPath, sizeof(neogitPath), "%s\\neogit", currentDirectory);
+        if (initexist(neogitPath))
+        {
+            printf("Error: 'neogit' folder already exists in this directory or an upper directory.\n");
+            return 1;
+        }
+        snprintf(neogitPath, sizeof(neogitPath), "%s\\..", currentDirectory);
+        if (GetFullPathName(neogitPath, MAX_PATH, (LPSTR)currentDirectory, NULL) == 0)
+        {
+            printf("Error getting full path name.\n");
+            return 1;
+        }
+        if (_stricmp(currentDirectory, "C:\\Users") == 0)
+            break;
+    }
+    return 0;
 }
 void init()
 {
     char currentDirectory[MAX_PATH];
     GetCurrentDirectory(MAX_PATH, currentDirectory);
-
     char neogitPath[MAX_PATH];
     snprintf(neogitPath, sizeof(neogitPath), "%s\\neogit", currentDirectory);
-
-    if (init_exist(neogitPath))
-    {
-        printf("Error: 'neogit' folder already exists in this directory.\n");
+    if (initcheckup(currentDirectory))
         return;
-    }
-
-    char upperDirectory[MAX_PATH];
-    snprintf(upperDirectory, sizeof(upperDirectory), "%s\\..\\neogit", currentDirectory);
-
-    if (init_exist(upperDirectory))
-    {
-        printf("Error: 'neogit' folder already exists in the upper directory.\n");
-        return;
-    }
-
     if (CreateDirectory(neogitPath, NULL) || ERROR_ALREADY_EXISTS == GetLastError())
     {
         DWORD attributes = GetFileAttributes(neogitPath);
         SetFileAttributes(neogitPath, attributes | FILE_ATTRIBUTE_HIDDEN);
-        printf("Repository 'neogit' created and hidden.\n");
+        printf("Repos 'neogit' created.\n");
+
+        char staged[MAX_PATH];
+        snprintf(staged, sizeof(staged), "%s\\staged", neogitPath);
+        CreateDirectory(staged, NULL);
+
+        char unstaged[MAX_PATH];
+        snprintf(unstaged, sizeof(unstaged), "%s\\unstaged", neogitPath);
+        CreateDirectory(unstaged, NULL);
     }
     else
-    {
-        printf("Error creating 'neogit' repository.\n");
-    }
+        printf("Error creating 'neogit'.\n");
 }
 
 int main(int argc, char *argv[])
@@ -57,6 +70,5 @@ int main(int argc, char *argv[])
     {
         printf("Unknown command: %s\n", argv[1]);
     }
-
     return 0;
 }
